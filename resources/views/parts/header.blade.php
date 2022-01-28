fd9696145a8501bdce17e3f62b63097c2069f868
mohp <miho.kj8.s@gmail.com>
2022-01-28 13:29:33 +0000

    1/28

--- resources/views/parts/header.blade.php
+++ resources/views/parts/header.blade.php
@@ -1,7 +1,12 @@
 <header>
-	<h1>美しい日本に出会う旅</h1>
-		<a href="/">トップ</a>
-		<a href="/list">投稿一覧</a>
-		<a href="/photo">絶景写真</a>
+	<link rel="stylesheet" href="{{  asset('css/style.css') }}" />
+	<h1　class="logo">美しい日本に出会う旅</h1>
 	@include("parts.nav")
+	<br>
+	<br>
+		<ul class="header-navigation">
+			<li><a href="/" class="main-nav">トップ</a></li>
+			<li><a href="/list" class="mai-nav">投稿一覧</a></li>
+			<li><a href="/photo" class="main-nav">絶景写真</a></li>
+		</ul>
 </header>
--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -3,14 +3,18 @@
     <head>
         <link rel="stylesheet" href="{{  asset('css/style.css') }}" />
     </head>
-    <body>
-        @include("parts.header")
-        <main>
-    	    <h1>これはトップページです</h1>
-                    <div class="topphoto">
-                      <img src="{{ asset('imgge/topphoto.jpg') }}" />
-                    </div>
-        </main>
-        @include("parts.footer")
-    </body>
+        <body>
+            @include("parts.header")
+            <main>
+            	<h1>これはトップページです</h1>
+                <div class="top-photo">       
+                    <img width="1550" height="700" src="{{ asset('image/topphoto.jpg') }}" alt="">
+            	</div>
+            <br>
+            <br>
+            	<div><h4>新着情報</h4>
+            	</div>
+            </main>
+    @include("parts.footer")
+        </body>
 </html>
