36b05ab296c627b7b1086bf80fa1868085818d06
mohp <miho.kj8.s@gmail.com>
2022-02-08 13:11:31 +0000

    2/8

--- resources/views/photo.blade.php
+++ resources/views/photo.blade.php
@@ -1,13 +1,34 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <link rel="stylesheet" href="{{  asset('css/style.css') }}" />
-    </head>
-    <body>
-        @include("parts.header")
-        <main>
-    	    <h1>写真</h1>
-        </main>
-        @include("parts.footer")
-    </body>
-</html>
+@include("parts.header")
+<main>
+    <h4>日本の絶景</h4>
+	
+</main>
+	<div class="card-contents">
+			<div class="list-area">
+				<div class="list">
+					<div class="photo-list">
+						<img src="image/上高地.jpg">
+						<p class="news-text">上高地</p>
+					</div>
+				</div>
+				<div class="list">
+					<div class="photo-list">
+						<img src="image/にこ淵.jpg">
+						<p class="news-text">にこ淵</p>
+					</div>
+				</div>
+				<div class="list">
+					<div class="photo-list">
+						<img src="image/父母ヶ浜.jpeg">
+						<p class="news-text">父母ヶ浜</p>
+					</div>
+				<div class="list">
+					<div class="photo-list">
+						<img src="image/山口県.jpg">
+						<p class="news-text">元乃隅稲成神社</p>
+					</div>
+				</div>
+			</div>
+		</div>
+
+@include("parts.footer")
