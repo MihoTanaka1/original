f2e598442671a8bafa408502e443ff2972bc18b5
mohp <miho.kj8.s@gmail.com>
2022-03-04 12:22:23 +0000

    3/4

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -48,6 +48,53 @@
 						</div>
 					</a>
 				</div>
+			</div>
 		</div>
-	</div>	
+				<div>
+			<div class="news">PICK UP</div>
+			<div class="news-bottom">ピックアップ</div>
+		</div>
+		<div class="card-contents">
+			<div class="list-area">
+				<div class="list">
+					<a href="AA">
+						<div class="image-list">
+							<div class="image-list-bg"></div>
+							<img src="image/写真1.JPG">
+						</div>
+						<div class="text-list">
+							<p class="photo-title">2021.11.4</p>
+							<p class="news-text">長野</p>
+						</div>
+					</a>
+				</div>
+				<div class="list">
+					<a href="AA">
+						<div class="image-list">
+							<div class="image-list-bg"></div>
+							<img src="image/写真2.JPG">
+						</div>
+						<div class="text-list">
+							<p class="photo-title">202.114</p>
+							<p class="news-text">石垣島</p>
+						</div>
+					</a>
+				</div>
+				<div class="list">
+					<a href="AA">
+						<div class="image-list">
+							<div class="image-list-bg"></div>
+							<img src="image/写真3.jpg">
+						</div>
+						<div class="text-list">
+							<p class="photo-title">2020.8.11</p>
+							<p class="news-text">立山</p>
+						</div>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
 	@endsection
